4a6679cbf290433bc7a3101413e9030f9b0860b9
Chaithanya Heblikar <csheblikar@gmail.com>
2024-07-04 15:00:05 -0700

    change connected log

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 (async () => {
   await mongoose.connect(process.env.MONGO_URI);
-  console.log(`Connected to ${process.env.MONGO_URI}...`);
+  console.log(`Connected to MongoDB...`);
 
   const port = process.env.PORT || 4000;
   app.listen(port, () => console.log(`Listening on port ${port}...`));
